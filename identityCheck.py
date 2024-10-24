5886d7af7fbdcb4e616d962d9b5e3cba8bff01c0
anttijelekainen00 <anttijelekainen789@gmail.com>
2024-10-24 12:53:19 +0300

    Poistettu TODO:t ja lisätty järkevyystarkistukset

--- identityCheck.py
+++ identityCheck.py
@@ -154,7 +154,6 @@ def checkHeTu(hetu):
 
     return result
 
-# TODO: Poista loput rivit, kun valmista!
 # KOKEILLAAN ERILAISIA VAIHTOEHTOJA
 # ---------------------------------
 
--- identityCheck2.py
+++ identityCheck2.py
@@ -4,6 +4,9 @@
 # KIRJASTOT JA MODUULIT
 # ---------------------
 
+# Kirjasto päivämäärälaskentaa varten
+import datetime
+
 # LUOKAT
 # ------
 
@@ -24,6 +27,47 @@ class NationalSSN:
         self.number = 0
         self.gender = ''
         self.checkSum = ''
+
+        # Sanakirjat vuosisatakoodeille ja varmisteille
+        self.centuryCodes = {
+        '+': '1800',
+        '-': '1900',
+        'A': '2000'
+        }   
+
+        self.modulosSymbols = {
+        0: '0',
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F',
+        16: 'H',
+        17: 'J',
+        18: 'K',
+        19: 'L',
+        20: 'M',
+        21: 'N',
+        22: 'P',
+        23: 'R',
+        24: 'S',
+        25: 'T',
+        26: 'U',
+        27: 'V',
+        28: 'W',
+        29: 'X',
+        30: 'Y'
+        }
 
     # Luokan metodit eri osien laskentaan ja järkevyystarkistuksiin
 
@@ -58,7 +102,7 @@ class NationalSSN:
             birthNumberPart = self.ssn[7:10]
             checksumPart = self.ssn[10]
             return {'days': dayPart,
-                    'monhts': monthPart,
+                    'months': monthPart,
                     'years': yearPart,
                     'century': centuryPart,
                     'number': birthNumberPart,
@@ -69,20 +113,47 @@ class NationalSSN:
             return {'status' : 'error'}
 
     # Muutetaan syntymäaikaosa ja vuosisata päivämääräksi
-    def getDateOfBirth(self, arg):
-        pass
+    def getDateOfBirth(self) -> None:
+        """Sets the value of DateOfBirth property for object
+        """
+        if self.checkSsnLenghtOk():
+            isoDate = '1799-12-31'
+            parts = self.splitSsn()
+            centurySymbol = parts['century']
+            century = self.centuryCodes[centurySymbol]
+            isoDate = century[0:2] + parts['years'] + '-' + parts['months'] + '-' + parts['days']
+            self.dateOfBirth = isoDate
 
     # Lasketaan ikä nyt täysinä vuosina
-    def calculateAge(self, arg):
+    def calculateAge(self):
         pass
 
     # Selvitetään varmistussumman avulla onko HeTu syötetty oikein
-    def isValidSsn(self, arg):
-        pass
+    def isValidSsn(self) -> bool:
+        """Recalculates the checksum of the SSN and verifies it is the same in the given SSN
 
-# MAIN KOKEILUJA VARTE (POISTA KUN EI ENÄÄ TARVITA)
+        Returns:
+            bool: True if SSN is valid, False otherwise
+        """
+        if self.checkSsnLenghtOk:
+            parts = self.splitSsn()
+            moduloString = parts['days'] + parts['months'] + parts['years'] + parts['number']
+            moduloNumeric = int(moduloString)
+            checkSumCalculated = moduloNumeric % 31
+            checkSumCalculatedSymbol = self.modulosSymbols[checkSumCalculated]
+            if checkSumCalculatedSymbol == parts['checksum']:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+# MAIN KOKEILUJA VARTEN (POISTA KUN EI ENÄÄ TARVITA)
 #=====================
 if __name__ == "__main__":
     hetu1 = NationalSSN('130728-478N')
+    hetu1.getDateOfBirth()
     print('Oikein muodostettu:', hetu1.checkSsnLenghtOk())
     print('HeTun osat ovat:', hetu1.splitSsn())
+    print('Syntymäaikaosa ISO-muodossa on: ', hetu1.dateOfBirth)
+    print('Henkilötunnus on oikein muodostettu', hetu1.isValidSsn())
