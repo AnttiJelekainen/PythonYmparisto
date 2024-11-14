5d3f0d5cc5237d049d14ffbaed7228565b4fc43a
anttijelekainen00 <anttijelekainen789@gmail.com>
2024-11-14 20:40:53 +0200

    app torstain 24.10.2024 tilanne.

--- app.py
+++ app.py
@@ -16,26 +16,39 @@ import identityCheck2
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0.
 
 
-# TODO: Pääohjelman ikuinen silmukka, josta poistutaan tarvittaessa (keksi mekanismi itse)
-# TODO:  Paranna pääohjelma siten, että se ei kaadu, kun käyttäjä syöttää virheellisen henkilötunnuksen
-userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
-userGivenLastName = input('Syötä asiakkaan sukunimi: ')
-# TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
-userGivenFirstName = input('Syötä asiakkaan etunimi: ')
-# TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
-# TODO: Varaudu tilanteeseen, jossa hetu:n tarkiste on annettu pienillä kirjaimilla
-# TODO: Muuta syötettyjen nimien alkukirjain isoksi
+while True:
+    userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+    userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
 
-ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
-if ssnToCheck.isValidSsn() == True:
-    dateOfBirth = ssnToCheck.getDateOfBirth()
-    gender = ssnToCheck.getGender()
-    age = ssnToCheck.calculateAge()
-    print('Syntymäaika: ', ssnToCheck.dateOfBirth)
-    print('Sukupuoli: ', ssnToCheck.gender)
-    print('Ikä: ', age)
+    # TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
 
-# Käynnistetään videokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
-#sound.parametricBeep(400, 330)
-#sound.playWav('Alkaa.WAV')
+    # TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> reg exp
+
+    ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
+    if ssnToCheck.isValidSsn() == True:
+        try:
+            ssnToCheck.getDateOfBirth()
+            ssnToCheck.getGender()
+            age = ssnToCheck.calculateAge()
+            userGivenLastName = input('Syötä asiakkaan sukunimi: ')
+            userGivenLastName = userGivenLastName.capitalize()
+            userGivenFirstName = input('Syötä asiakkaan etunimi: ')
+            userGivenFirstName = userGivenFirstName.capitalize()
+            print('Asiakkaan nimi on:', userGivenLastName, userGivenFirstName)
+            print('Syntymäaika: ', ssnToCheck.dateOfBirth)
+            print('Sukupuoli: ', ssnToCheck.gender)
+            print('Ikä: ', age)
+        except Exception as e:
+            print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
+
+
+    # Kysytään halutaanko poistua ohjelmasta
+    wantAbort = input('Haluatko päättää ohjelman k/E:')
+    # Muutetaan vastaus isoksi kirjaimiksi ja tarkastetaan kirjain K
+    if wantAbort.upper() == 'K':
+        break
+
+    # Käynnistetään videokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
+    #sound.parametricBeep(400, 330)
+    #sound.playWav('Alkaa.WAV')
 
