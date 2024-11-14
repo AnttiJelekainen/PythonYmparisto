a095f64d9071a614517b6d0611ee02ef0021ae15
anttijelekainen00 <anttijelekainen789@gmail.com>
2024-11-14 20:02:23 +0200

    Korjattu virheellinen koodi ja lisätty TODO:t

--- app.py
+++ app.py
@@ -19,13 +19,12 @@ kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0.
 # TODO: Pääohjelman ikuinen silmukka, josta poistutaan tarvittaessa (keksi mekanismi itse)
 # TODO:  Paranna pääohjelma siten, että se ei kaadu, kun käyttäjä syöttää virheellisen henkilötunnuksen
 userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
-userGivenLastName = input('Syötä asiakkaan sukunimi')
+userGivenLastName = input('Syötä asiakkaan sukunimi: ')
 # TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
-userGivenFirstName = input('Syötä asiakkaan etunimi')
+userGivenFirstName = input('Syötä asiakkaan etunimi: ')
 # TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
 # TODO: Varaudu tilanteeseen, jossa hetu:n tarkiste on annettu pienillä kirjaimilla
 # TODO: Muuta syötettyjen nimien alkukirjain isoksi
-userGivenFirstName = input('Syötä asiakkaan etunimi')
 
 ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
 if ssnToCheck.isValidSsn() == True:
